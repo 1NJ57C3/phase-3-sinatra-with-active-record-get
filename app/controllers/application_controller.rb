c0c6868520e500f5ae9561f9164fcd344ae85fa1
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2022-01-24 10:44:47 -0800

    commit tree d36052bd2d47daca297ccd37a8dd8b45401cd025

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,38 @@
 class ApplicationController < Sinatra::Base
 
+  # This line to set Content-Type header for all responses
+  set :default_content_type, 'application/json'
+
   get '/' do
     { message: "Hello world" }.to_json
+  end
+
+  get '/games' do
+    # get all the games from the database (using ActiveRecord)
+    # games = Game.all
+
+    # games = Game.all.order(:title) # Sort games by title instead of default sort order (ID)
+
+    # games = Game.all.order(:title).limit(10) # Return just first 10 results
+
+    # return a JSON response with an array of all the game data
+    # games.to_json
+    Game.all.to_json # Side-test: Direct method chaining also works.
+  end
+
+  get '/games/:id' do
+    # look up the game in the database using its ID
+    game = Game.find(params[:id])
+    # send a JSON-formatted response of the game data
+    # game.to_json
+    # include associated reviews in the JSON response
+    # game.to_json(include: :reviews)
+    # include associated users with each review
+    # game.to_json(include: { reviews: { include: :user } })
+    # selectivity re: which attributes are returned from each model via ONLY
+    game.to_json(only: [:id, :title, :genre, :price], include: { reviews: { only: [:comment, :score], include: { user: {only: [:name] }
+      } }
+    })
   end
 
 end
